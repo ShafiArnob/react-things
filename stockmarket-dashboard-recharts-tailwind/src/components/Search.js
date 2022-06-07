193d8ccb21384063813463ba1303e2fa7a1a4204
ShafiArnob <shafiarnob@gmail.com>
2022-06-07 23:08:45 +0600

    stockmarket-dashboard -- searchresults

--- stockmarket-dashboard-recharts-tailwind/src/components/Search.js
+++ stockmarket-dashboard-recharts-tailwind/src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { mockSearchResults } from '../constants/mock';
 import {XIcon, SearchIcon} from "@heroicons/react/solid"
+import SearchResults from './SearchResults';
 const Search = () => {
   const [input, setInput] = useState("");
 
@@ -29,13 +30,15 @@ const Search = () => {
         }}
         />
 
-        <button>
+        <button className='m-1'>
           {input && <XIcon className='h-4 w-4 fill-gray-500'></XIcon>}
         </button>
 
         <button onClick={updateBestMatches} className="h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2">
           <SearchIcon className='h-4 w-4 fill-gray-100'></SearchIcon>
         </button>
+
+        {input && bestMatches.length>0 ? <SearchResults results={bestMatches}></SearchResults>:null}
     </div>
   );
 };
